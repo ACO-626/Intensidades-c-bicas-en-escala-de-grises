4890f86d17096c2004a573b414bead34df54bbeb
Alejandro Ocampo Cisneros <ocampoalejandro.oc@gmail.com>
2021-01-14 14:47:12 -0600

    Agregar archivos de proyecto.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace TemasSelectosdeProgramacióneImagenes
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnTransformar = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.xDos = new System.Windows.Forms.TextBox();
+            this.xtres = new System.Windows.Forms.TextBox();
+            this.X3 = new System.Windows.Forms.Label();
+            this.labelx2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.xZero = new System.Windows.Forms.TextBox();
+            this.xUno = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.pictureBoxDemo = new System.Windows.Forms.PictureBox();
+            this.grafica = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDemo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grafica)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(713, 114);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(447, 282);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(44, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "IMPORTAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnTransformar
+            // 
+            this.btnTransformar.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnTransformar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTransformar.Enabled = false;
+            this.btnTransformar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTransformar.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTransformar.Location = new System.Drawing.Point(1070, 12);
+            this.btnTransformar.Name = "btnTransformar";
+            this.btnTransformar.Size = new System.Drawing.Size(140, 48);
+            this.btnTransformar.TabIndex = 3;
+            this.btnTransformar.Text = "TRANSFORMAR";
+            this.btnTransformar.UseVisualStyleBackColor = false;
+            this.btnTransformar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(713, 418);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(447, 282);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // xDos
+            // 
+            this.xDos.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xDos.Location = new System.Drawing.Point(467, 20);
+            this.xDos.Name = "xDos";
+            this.xDos.Size = new System.Drawing.Size(103, 30);
+            this.xDos.TabIndex = 5;
+            this.xDos.TextChanged += new System.EventHandler(this.xDos_TextChanged);
+            // 
+            // xtres
+            // 
+            this.xtres.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xtres.Location = new System.Drawing.Point(296, 19);
+            this.xtres.Name = "xtres";
+            this.xtres.Size = new System.Drawing.Size(103, 30);
+            this.xtres.TabIndex = 6;
+            this.xtres.TextChanged += new System.EventHandler(this.xtres_TextChanged);
+            // 
+            // X3
+            // 
+            this.X3.AutoSize = true;
+            this.X3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.X3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.X3.Location = new System.Drawing.Point(405, 24);
+            this.X3.Name = "X3";
+            this.X3.Size = new System.Drawing.Size(54, 25);
+            this.X3.TabIndex = 7;
+            this.X3.Text = "X3 +";
+            // 
+            // labelx2
+            // 
+            this.labelx2.AutoSize = true;
+            this.labelx2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelx2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelx2.Location = new System.Drawing.Point(576, 25);
+            this.labelx2.Name = "labelx2";
+            this.labelx2.Size = new System.Drawing.Size(49, 25);
+            this.labelx2.TabIndex = 8;
+            this.labelx2.Text = "X2+";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(183, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 31);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Original";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Location = new System.Drawing.Point(878, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 31);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Nueva";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.xZero);
+            this.panel1.Controls.Add(this.xUno);
+            this.panel1.Controls.Add(this.label);
+            this.panel1.Controls.Add(this.X3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.labelx2);
+            this.panel1.Controls.Add(this.xtres);
+            this.panel1.Controls.Add(this.xDos);
+            this.panel1.Controls.Add(this.btnTransformar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1264, 73);
+            this.panel1.TabIndex = 11;
+            // 
+            // xZero
+            // 
+            this.xZero.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xZero.Location = new System.Drawing.Point(822, 19);
+            this.xZero.Name = "xZero";
+            this.xZero.Size = new System.Drawing.Size(103, 30);
+            this.xZero.TabIndex = 13;
+            this.xZero.TextChanged += new System.EventHandler(this.xZero_TextChanged);
+            // 
+            // xUno
+            // 
+            this.xUno.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xUno.Location = new System.Drawing.Point(643, 19);
+            this.xUno.Name = "xUno";
+            this.xUno.Size = new System.Drawing.Size(103, 30);
+            this.xUno.TabIndex = 12;
+            this.xUno.TextChanged += new System.EventHandler(this.xUno_TextChanged);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label.Location = new System.Drawing.Point(752, 23);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(49, 25);
+            this.label.TabIndex = 9;
+            this.label.Text = "X1+";
+            // 
+            // pictureBoxDemo
+            // 
+            this.pictureBoxDemo.Location = new System.Drawing.Point(263, 143);
+            this.pictureBoxDemo.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBoxDemo.Name = "pictureBoxDemo";
+            this.pictureBoxDemo.Size = new System.Drawing.Size(217, 140);
+            this.pictureBoxDemo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxDemo.TabIndex = 13;
+            this.pictureBoxDemo.TabStop = false;
+            // 
+            // grafica
+            // 
+            this.grafica.BackColor = System.Drawing.Color.DimGray;
+            this.grafica.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight;
+            this.grafica.BackImageTransparentColor = System.Drawing.Color.White;
+            this.grafica.BackSecondaryColor = System.Drawing.Color.WhiteSmoke;
+            this.grafica.BorderlineColor = System.Drawing.Color.Maroon;
+            this.grafica.BorderlineWidth = 5;
+            this.grafica.BorderSkin.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.grafica.BorderSkin.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            this.grafica.BorderSkin.PageColor = System.Drawing.Color.Maroon;
+            chartArea4.Name = "ChartArea1";
+            this.grafica.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.grafica.Legends.Add(legend4);
+            this.grafica.Location = new System.Drawing.Point(148, 335);
+            this.grafica.Name = "grafica";
+            series4.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight;
+            series4.BackImageTransparentColor = System.Drawing.Color.Maroon;
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Color = System.Drawing.Color.Black;
+            series4.LabelBackColor = System.Drawing.Color.White;
+            series4.Legend = "Legend1";
+            series4.MarkerColor = System.Drawing.Color.White;
+            series4.Name = "f";
+            this.grafica.Series.Add(series4);
+            this.grafica.Size = new System.Drawing.Size(434, 282);
+            this.grafica.TabIndex = 14;
+            this.grafica.Text = "chart1";
+            this.grafica.Visible = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(1264, 711);
+            this.Controls.Add(this.grafica);
+            this.Controls.Add(this.pictureBoxDemo);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Editor gráfico cúbico";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDemo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grafica)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnTransformar;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox xDos;
+        private System.Windows.Forms.TextBox xtres;
+        private System.Windows.Forms.Label X3;
+        private System.Windows.Forms.Label labelx2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox xZero;
+        private System.Windows.Forms.TextBox xUno;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.PictureBox pictureBoxDemo;
+        private System.Windows.Forms.DataVisualization.Charting.Chart grafica;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace TemasSelectosdeProgramacióneImagenes
+{
+    public partial class FormMain : Form
+    {
+        Image<Bgr, Byte> My_Image;
+        Image<Bgr, Byte> DemoImage;
+        int y = 0;
+        double ygraf = 0;
+     
+
+        public FormMain()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                My_Image = new Image<Bgr, byte>(Openfile.FileName);
+                Image<Gray, Byte> My_gray = My_Image.Convert<Gray, byte>();
+                Image<Bgr, Byte> My_Demo = My_Image.Convert<Bgr, byte>();
+
+                pictureBox1.Image = My_gray.ToBitmap();
+                pictureBox2.Image = My_gray.ToBitmap();
+                pictureBoxDemo.Image = My_Demo.ToBitmap();
+
+                xtres.Text = "0";
+                xDos.Text = "0";
+                xUno.Text = "1";
+                xZero.Text = "0";
+
+                btnTransformar.Enabled = true;
+                grafica.Visible = true;
+                graficar();
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                
+                   Image<Gray, byte> My_gray2 = My_Image.Convert<Gray, byte>();
+                    for (int i=0;i<My_Image.Height;i++)
+                    {
+                        for(int j=0;j<My_Image.Width; j++)
+                        {
+                        //My_gray2[i, j] = new Gray(nuevogris(float.Parse(xtres.Text),float.Parse(xDos.Text),(int)My_gray2[i,j].Intensity));
+                        My_gray2[i, j] = new Gray(nuevogris((int)My_gray2[i, j].Intensity));
+                        }
+                       
+                    }
+                    pictureBox2.Image = My_gray2.ToBitmap();
+                    graficar();
+
+                       
+               
+            }catch(IOException)
+            {
+                MessageBox.Show("Error al cargar");
+            }
+
+        }
+        private int nuevogris(int i)
+        {
+            y = (int)(Math.Pow(i, 3) * float.Parse(xtres.Text) + Math.Pow(i, 2) * float.Parse(xDos.Text) + Math.Pow(i, 1) * float.Parse(xUno.Text) + float.Parse(xZero.Text));
+
+            if (y>=255)
+            {
+                y = 255;
+            }
+            if(y<=0)
+            {
+                y = 0;
+            }
+            return y;
+        }
+
+        private void graficar()
+        {
+            foreach(var series in grafica.Series)
+            {
+                series.Points.Clear();
+            }
+
+            for(int i=0;i<255;i++)
+            {
+                //grafica.Visible = true;
+                ygraf = Math.Pow(i, 3) * float.Parse(xtres.Text) + Math.Pow(i, 2) * float.Parse(xDos.Text) + Math.Pow(i, 1) * float.Parse(xUno.Text) + float.Parse(xZero.Text);
+                grafica.Series["f"].Points.AddXY(i, ygraf);
+                /*if(ygraf>255|ygraf<0)
+                {
+                    
+                }else
+                {
+                    grafica.Series["f"].Points.AddXY(i, ygraf);
+                }*/
+            }
+        }
+
+        private void xtres_TextChanged(object sender, EventArgs e)
+        {
+            if (xtres.Text == "") 
+                xtres.Text = "0";
+            
+        }
+
+        private void xDos_TextChanged(object sender, EventArgs e)
+        {
+            if (xDos.Text == "")
+                xDos.Text = "0";
+           
+           
+        }
+
+        private void xUno_TextChanged(object sender, EventArgs e)
+        {
+            if (xUno.Text == "")
+                xUno.Text = "1";
+        }
+
+        private void xZero_TextChanged(object sender, EventArgs e)
+        {
+            if (xZero.Text == "")
+                xZero.Text = "0";
+        }
+    }
+}
